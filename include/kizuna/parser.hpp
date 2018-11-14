cdd7ec1f0377c8202f582dbea301bdf0d702ba94
ankoton <10003601+a@users.noreply.github.com>
2018-11-14 23:38:09 +0900

    ？

--- include/kizuna/parser.hpp
+++ include/kizuna/parser.hpp
@@ -6,16 +6,21 @@
 #include <kizuna/input_stream.hpp>
 #include <kizuna/exception.hpp>
 
+
+#define KIZUNA_ERROR_DEF(name,message) \
+struct name : exception\
+{\
+  name (const input_stream & x)\
+    : exception {x.generate_error ( message )}\
+  {}\
+  using exception::what;\
+}
+
 namespace kizuna::parser
 {
 
-  struct eof_error : exception
-  {
-    eof_error (const input_stream & x)
-      : exception {x.generate_error ("expected EOF, but not EOF")}
-    {}
-    using exception::what;
-  };
+  KIZUNA_ERROR_DEF (eof_error , "expected EOF , but not EOF");
+  KIZUNA_ERROR_DEF (failed_parse_string_error , "parse failed");
 
 
   struct not_decimal_int_literal_error : exception
@@ -81,6 +86,9 @@ namespace kizuna::parser
   };
 
 
+  KIZUNA_ERROR_DEF (not_bool_literal_error , "not bool literal");
+
+
   struct unknown_literal_suffix_error : exception
   {
     unknown_literal_suffix_error (const input_stream & x)
@@ -122,6 +130,43 @@ namespace kizuna::parser
   inline auto parse_anychar (input_stream & s)
   {
     return s.get ();
+  }
+
+
+  inline auto parse_string (const std::string & str , input_stream & s)
+  {
+    // auto backup = s;
+    for (auto && elem : str)
+    {
+      if (s.peek () != elem)
+      {
+        // s = backup;
+        throw failed_parse_string_error {s};
+      }
+      s.skip1 ();
+    }
+    return str;
+  }
+
+
+  KIZUNA_ERROR_DEF (not_keyword_error , "not keyword");
+
+  inline auto parse_keyword (const std::string & str , input_stream & s)
+  {
+    s.skip_spaces ();
+    for (auto && elem : str)
+    {
+      if (s.peek () != elem)
+      {
+        throw not_keyword_error {s};
+      }
+      s.skip1 ();
+    }
+    if (s.is_letter_tail ())
+    {
+      throw not_keyword_error {s};
+    }
+    return str;
   }
 
 
@@ -347,6 +392,83 @@ namespace kizuna::parser
   }
 
 
+  inline auto parse_bool_literal (input_stream & s)
+  {
+    s.skip_spaces ();
+    auto backup = s;
+
+    try
+    {
+      auto res = parse_keyword ("true" , s);
+      return res;
+    }
+    catch (const not_keyword_error &)
+    {
+      s = backup;
+    }
+
+    try
+    {
+      auto res = parse_keyword ("false" , s);
+      return res;
+    }
+    catch (const not_keyword_error &)
+    {
+      throw not_bool_literal_error {s};
+    }
+  }
+
+
+  KIZUNA_ERROR_DEF (not_string_literal_error , "not string literal");
+
+
+  inline auto parse_string_literal (input_stream & s)
+  {
+    s.skip_spaces ();
+    auto begin = s.get_iterator ();
+
+    if (s.peek () == '"')
+    {
+      s.skip1 ();
+    }
+    else
+    {
+      throw not_string_literal_error {s};
+    }
+
+    while (! s.is_eof () && ! s.is_endline () && s.peek () != '"')
+    {
+      if (s.peek () == '\\')
+      {
+        s.skip1 ();
+        if (! s.is_eof () && ! s.is_endline ())
+        {
+          s.skip1 ();
+        }
+        else
+        {
+          throw not_string_literal_error {s};
+        }
+      }
+      else
+      {
+        s.skip1 ();
+      }
+    }
+
+    if (s.peek () == '"')
+    {
+      s.skip1 ();
+    }
+    else
+    {
+      throw not_string_literal_error {s};
+    }
+
+    return std::string {begin , s.get_iterator ()};
+  }
+
+
   inline auto parse_literal (input_stream & s)
   {
     auto backup = s;
@@ -365,6 +487,24 @@ namespace kizuna::parser
       return parse_int_literal (s);
     }
     catch (const not_int_literal_error &)
+    {
+      s = backup;
+    }
+
+    try
+    {
+      return parse_bool_literal (s);
+    }
+    catch (const not_bool_literal_error &)
+    {
+      s = backup;
+    }
+
+    try
+    {
+      return parse_string_literal (s);
+    }
+    catch (const not_string_literal_error &)
     {
       // s = backup;
     }
@@ -398,7 +538,9 @@ namespace kizuna::parser
     return std::string {begin , s.get_iterator ()};
   }
 
-};
+}
+
+#undef KIZUNA_ERROR_DEF
 
 #endif
 
